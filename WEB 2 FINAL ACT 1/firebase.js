4beb4654348f621c2013306dcf1b2232d938cbac
RicoTacmual <123869561+RicoTacmual@users.noreply.github.com>
2023-04-29 15:13:54 +0800

    Add files via upload

---
+++ WEB 2 FINAL ACT 1/firebase.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1uRm4PEGnfxQP0mOJ2sMo9DYzuf6XyYE",
+    authDomain: "final-activity-1.firebaseapp.com",
+    databaseURL: "https://final-activity-1-default-rtdb.firebaseio.com",
+    projectId: "final-activity-1",
+    storageBucket: "final-activity-1.appspot.com",
+    messagingSenderId: "968430786060",
+    appId: "1:968430786060:web:abbbd40d8224eb201a8029",
+    measurementId: "G-NGX5BF0Z10"
+  };
+
+  firebaseConfig.initailizeApp(firebaseConfig);
+
+  import index from './WEB 2 FINAL ACT 1/index'
+    export default function home(){
+        return {
+           
+        }
+    }
